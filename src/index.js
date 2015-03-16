bcdeb26f2aae3e2b6877650a9ac4d45d4dffb107
Tim Branyen <tim@tabdeveloper.com>
2015-03-16 19:37:28 -0400

    Fix up package.json and simplify example
    Share `console.log` and use relative file path to load.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ define(function(require, exports, module) {
   'use strict';
 
   var combyne = require('combyne');
-  var output = combyne('It {{ lol }}', { lol: 'works' }).render();
+  var output = combyne('It {{ lol }}!', { lol: 'works' }).render();
 
-  if (typeof document !== 'undefined') {
-    document.body.innerHTML = output;
-  }
-  else {
-    console.log(output);
-  }
+  console.log(output);
 });
